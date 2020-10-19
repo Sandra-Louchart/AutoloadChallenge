643ebbc2b53bb3aae85f402e7718a05f77f026bc
Sandra-Louchart <sandra.louchart@gmail.com>
2020-10-19 11:49:34 +0200

    correction

--- public/index.php
+++ public/index.php
@@ -1,2 +1,6 @@
 <?php
 require 'vendor/autoload.php';
+
+use App\Wcs\Hello;
+$hello = new Hello ();
+echo $hello->talk();
--- src/Wcs/Hello.php
+++ src/Wcs/Hello.php
@@ -1,2 +1,9 @@
 <?php
-return 'Hello World'
+namespace App\Wcs;
+class Hello
+{
+    public function talk(): string
+    {
+        return "Hello World!";
+    }
+}
